719a65d9ddfde2e1efad1e6b9c04973a607531d8
卢俊 <lujun.hust@gmail.com>
2018-03-08 21:47:13 +0800

    changed the license to LGPLv2

--- Java/Tester.java
+++ Java/Tester.java
@@ -5,9 +5,10 @@
  *  
  *  https://github.com/Jhuster/TLV
  *   
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; version 2 of the License.  
+ *  This library is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published
+ *  by the Free Software Foundation; either version 2.1 of the License, 
+ *  or (at your option) any later version.
  */
 public class Tester {
 
--- c/key_list.c
+++ c/key_list.c
@@ -1,13 +1,14 @@
 /*
  *  COPYRIGHT NOTICE  
  *  Copyright (C) 2015, Jhuster, All Rights Reserved
- *  Author:  Jhuster(lujun.hust@gmail.com)
+ *  Author: Jhuster(lujun.hust@gmail.com)
  *  
  *  https://github.com/Jhuster/TLV
  *   
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; version 2 of the License.  
+ *  This library is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published
+ *  by the Free Software Foundation; either version 2.1 of the License, 
+ *  or (at your option) any later version.
  */
 #include "key_list.h"
 
